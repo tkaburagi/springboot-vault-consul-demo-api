1714c7950411107db780c7573daccb8b56e97d09
Takayuki Kaburagi <t.kaburagi@me.com>
2019-05-03 20:07:50 +0900

    webed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +16,21 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+    @Autowired
+    DataSource dataSource;
 
+    @PostConstruct
+    private void postConstruct() throws Exception {
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery("SELECT CURRENT_USER();");
+            resultSet.next();
+
+            System.out.println("Connection works with User: " + resultSet.getString(1));
+
+            resultSet.close();
+        }
+    }
 }
